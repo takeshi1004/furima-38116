6466804e80e14c3fe7ac2a677138a1965c48e5cf
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-12 12:02:41 +0900

    deviseに関するビューファイルの設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def create
+    @user = User.new(configure_permitted_parameters)
+    if @user.save
+      redirect_to 'items#index'
+    else
+      render :create
+    end
+  end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
