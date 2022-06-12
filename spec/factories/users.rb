d1790fde96500a2c93256e8f426b1dcbd685d5c7
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-12 20:05:59 +0900

    FactoryBotでのユーザー情報の指定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,19 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name}
+
+    transient do
+      person = Gimei.name
+    end
+
+    nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    
+    first_name            {person.first.kanji}
+    last_name             {person.last.kanji}
+    first_name_kana       {person.first.katakana}
+    last_name_kana        {person.last.katakana}
+    birthday              {Faker.Date.birthday}
+
   end
 end
