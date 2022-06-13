e13b16cc06a2d27ef20a2cb18acb473d232e6f0f
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-13 15:42:36 +0900

    Itemモデルの作成、マイグレーション実行、active_hashの導入、リンク記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_12_015644) do
+ActiveRecord::Schema.define(version: 2022_06_13_054630) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "content", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "ship_charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "take_days_id", null: false
+    t.integer "total_price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_06_12_015644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
