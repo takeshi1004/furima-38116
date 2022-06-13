394fbe9b2bb9f794c9f092a6afa340850d0867ca
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-13 16:41:34 +0900

    AciveHashで使うモデルの作成と記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_charge
+  belongs_to :prefecture
+  belongs_to :take_days
+
+  validates :item_name, presence: true
+  validates :content, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "カテゴリーを選んでください" } 
+  validates :status_id, numericality: { other_than: 1, message: "商品の状態を選んでください"}
+  validates :ship_charge_id, numericality: {other_than: 1, message: "配送料の負担を選んでください"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "発送元の地域を選んでください"}
+  validates :take_days_id, numericality: { other_than: 1, message: "発送までの日数を選んでください"}
+
 end
