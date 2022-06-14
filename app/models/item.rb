e1e94810d2ce8e78a1b18ecf9159affb2eb06dda
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-14 19:24:11 +0900

    エラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
   VALID_PRICE_REGEX = /\A[0-9]+\z/
   validates :total_price, format: {with: VALID_PRICE_REGEX}, allow_blank: true
   validates :total_price, presence: true
-  validates :total_price, numericality: {only_integer: true, message: "は半角数字で入力してください"}, allow_blank: true
-  validates :total_price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は指定の範囲内で価格を入力してください"}, allow_blank: true
+  validates :total_price, numericality: {only_integer: true}, allow_blank: true
+  validates :total_price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, allow_blank: true
 
 end
