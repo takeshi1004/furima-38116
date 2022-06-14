130a1bc0fb609b6a38c0eb5f9aadda3480ee08a2
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-14 21:15:20 +0900

    エラーメッセージの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,4 +23,5 @@ class Item < ApplicationRecord
   validates :total_price, numericality: {only_integer: true}, allow_blank: true
   validates :total_price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, allow_blank: true
 
+  validates :user, presence: { message: "が存在しません"}
 end
