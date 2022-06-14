a4948a60a3f31988a49fa2ed241d391db6cab0e7
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-14 16:07:22 +0900

    ItemモデルのFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name   {Faker::Games::Pokemon.name}
+    content     {Faker::Lorem.sentence}
+    category    {Category.where("id > ?", 1).sample}
+    status      {Status.where("id > ?", 1).sample}
+    ship_charge {ShipCharge.where("id > ?", 1).sample}
+    prefecture  {Prefecture.where("id > ?", 1).sample}
+    take_days   {TakeDays.where("id > ?", 1).sample}
+    total_price {Faker::Number.within(range: 300..9999999)}
+    association :user
   end
 end
