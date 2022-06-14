236e5b7f68eda0593657616611d789cf82e53974
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-14 18:01:14 +0900

    ファクトリーファイルの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    item_name   {Faker::Games::Pokemon.name}
-    content     {Faker::Lorem.sentence}
-    category    {Category.where("id > ?", 1).sample}
-    status      {Status.where("id > ?", 1).sample}
-    ship_charge {ShipCharge.where("id > ?", 1).sample}
-    prefecture  {Prefecture.where("id > ?", 1).sample}
-    take_days   {TakeDays.where("id > ?", 1).sample}
-    total_price {Faker::Number.within(range: 300..9999999)}
+    item_name      {Faker::Name.last_name}
+    content        {Faker::Lorem.sentence}
+    category_id    {2}
+    status_id      {2}
+    ship_charge_id {2}
+    prefecture_id  {2}
+    take_days_id   {2}
+    total_price    {Faker::Number.within(range: 300..9999999)}
     association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
