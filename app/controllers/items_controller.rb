6a91ce020141bf69446cbabbbe07dcdb6f82d51f
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-15 20:10:13 +0900

    itemsコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, only: :new
+before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,9 +23,19 @@ before_action :authenticate_user!, only: :new
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :content, :category_id, :status_id, :ship_charge_id, :prefecture_id, :take_days_id, :total_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
