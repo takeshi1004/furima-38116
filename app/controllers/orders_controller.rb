739a30744d48265fe36c5e46653d52533b041603
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-18 18:28:56 +0900

    orders_controller.rbの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def new
@@ -7,18 +8,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_ship = OrderShip.new(order_params)
     if @order_ship.valid?
       @order_ship.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_ship).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :phone_number).merge(user_id: current_user.id, order_id: order.id)
+    params.permit(:postal_code, :prefecture_id, :city, :address1, :address2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
