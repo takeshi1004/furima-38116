135ce65d202564af80220b0d90d3c773b2de61dc
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-18 16:17:59 +0900

    マイグレーションファイルにカラム記入

---
+++ db/migrate/20220618064039_create_ships.rb
@@ -0,0 +1,14 @@
+class CreateShips < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ships do |t|
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address1, null: false
+      t.string :address2
+      t.string :phone_number, null: false
+      t.references :order, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
