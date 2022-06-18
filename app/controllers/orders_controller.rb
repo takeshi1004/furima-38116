abd17fd8b97dc133a1beaf96510713b8be6854ea
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-18 17:10:11 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,22 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order_ship = OrderShip.new
   end
 
   def create
-    
+    @order_ship = OrderShip.new(order_params)
+    if @order_ship.valid?
+      @order_ship.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_ship).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :phone_number).merge(user_id: current_user.id, order_id: order.id)
   end
 end
