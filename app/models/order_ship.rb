cc6bb56aaedfd41c09a0a7d398cebaf233551c59
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-19 17:28:47 +0900

    重複するエラーメッセージの編集

--- app/models/order_ship.rb
+++ app/models/order_ship.rb
@@ -8,12 +8,13 @@ class OrderShip
     validates :prefecture_id, numericality: { other_than: 1, message: "を選んでください" }
     validates :city
     validates :address1
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, messages: "が正しくありません" }
+    validates :phone_number
     validates :user_id
     validates :item_id
   end
 
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "にハイフン(-)を含めてください" }, allow_blank: true
+  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, messages: "が正しくありません" }, allow_blank: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
