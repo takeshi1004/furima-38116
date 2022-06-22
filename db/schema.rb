9e382d4c80a023b3b3787bfaa904d26160857c89
takeshi1004 <t.furesawa1994@gmail.com>
2022-06-22 18:11:59 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2022_06_22_083823) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "customer_token", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -103,6 +105,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_083823) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cards", "users"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
